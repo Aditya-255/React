22853a0adde9653b2c70050822fb8d93feb1f8d9
Aditya Dodiya <aadityadodiya01@gmail.com>
2025-12-24 17:50:13 +0530

    props Drilling

--- React_Tutorial/React_Sheryians_Tutorial/src/App.jsx
+++ React_Tutorial/React_Sheryians_Tutorial/src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import Card from './Component/Card'
+// import Card from './Component/Card'
+import { BsBookmark } from "react-icons/bs";
+import Card2 from './Component/Card2';
+
 
 // multiple return that time use fragment
 const App = () => {
@@ -14,14 +17,26 @@ const App = () => {
     //   </h1>
     // </div>
     // </>
-    <div>
-    <div className='card'>
-      <h1>React Tutorial</h1>
-      <Card />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum velit obcaecati, est dolorum voluptatibus autem laudantium fugiat illo eveniet id nihil soluta reiciendis, ex impedit? Quis distinctio cupiditate suscipit inventore?</p>
-    </div>
-    {Card()}
-    </div>
+    // <div>
+    // <div className='card'>
+    //   <h1>React Tutorial</h1>
+    //   <Card />
+    //   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum velit obcaecati, est dolorum voluptatibus autem laudantium fugiat illo eveniet id nihil soluta reiciendis, ex impedit? Quis distinctio cupiditate suscipit inventore?</p>
+    // </div>
+    // {Card()}
+    // </div>
+
+    // Understanding Props
+    //Properties
+      // <div className='parent'>
+      //   <Card user="Aditya"  age={18}/>
+      //   <Card user="ABC"/>
+      // </div>
+
+      //mini Project for props Drilling
+      <div className="parent">
+        <Card2 />
+      </div>
   )
 }
 
